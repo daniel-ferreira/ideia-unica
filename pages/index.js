91fd6ebe7c15ae296415c2b1fd5205c4104ef47e
daniel-o-ferreira <d.oliveira.ferreira@avanade.com>
2024-06-14 22:00:22 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,5 @@
-import { useState } from 'react';
-
 function Home(params) {
-    return (
-        <div>
-            <h3>Home</h3>
-            <Contador />
-        </div>
-    );
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);        
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    return <h1>Home</h1>
 }
 
 export default Home
